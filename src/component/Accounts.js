6d9b5c71be66eade06d18505e484bd756469b6d1
eukartsan <eugenekartsan@gmail.com>
2018-10-08 17:36:11 +0300

    add account name

--- src/component/Accounts.js
+++ src/component/Accounts.js
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 import AccountsNum from './AccountsNum';
 import './Accounts.css';
 
 export default class Accounts extends React.Component {
-    render(){
-      const {Scorelist} = this.props;
-      const arrayelem = Scorelist.map((item)=>{
-      const {id, ...itemProps} = item;
+    constructor() {
+        super()
 
-      return (
-        <li key={id} className="account-list list-group-item"><AccountsNum {...itemProps}/></li>
+        this.state = {
+            accountLabel: ''
+        };
+
+    }
+
+    addAccountLabel = (event) => {
+        const {addAccount} = this.props,
+            {accountLabel} = this.state;
+
+        event.preventDefault();
+
+        addAccount(accountLabel)
+
+        this.setState({
+            accountLabel: ''
+        })
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            accountLabel: event.target.value
+        });
+    }
+
+    render() {
+        const {accountsList} = this.props,
+            {accountLabel} = this.state
+
+        const account = accountsList.map((item) => {
+            const {id, label, total} = item;
+
+            return (
+                <li key={id} className="account-list list-group-item">
+                    <span>
+                        {label} : {total} UAH
+                    </span>
+                </li>
+            );
+        });
+
+        return (
+            <Fragment>
+                <div>
+                    <form onSubmit={this.addAccountLabel}>
+                        <label>
+                            Account name:
+                            <input type="text" value={accountLabel} onChange={this.handleChange}/>
+                        </label>
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+                <ul className="list-group ">
+                    {account}
+                </ul>
+            </Fragment>
         );
-      });
-
-      return (
-        <ul className="list-group ">
-          {arrayelem}
-        </ul>
-      );
     };
-  };
+};
--- src/index.js
+++ src/index.js
@@ -6,24 +6,48 @@ import Accounts from './component/Accounts';
 import Scoretransfer from './component/Scoretransfer';
 import './index.css';
 
-const App = () => {
-  const Scoreitem = [
-    {label: 'Score 1', total: '0', id: 1},
-    {label: 'Score 2', total: '20', id: 2},
-    {label: 'Score 3', total: '100', id: 3}
-  ];
+export default class App extends React.Component {
+    constructor() {
+        super()
 
-  return (
-    <div className="app-header">
-      <h1>Income and expense accounting application</h1>
-      <h3>Balance</h3>
-      <Accounts Scorelist={Scoreitem}/>
-        <h3>Account transactions: </h3>
-        <Operation/>
-        <Scoretransfer />
-    </div>
-  );
-};
+  this.state = {
+    accounts: [
+      {label: 'Score 1', total: '0', id: 1},
+      {label: 'Score 2', total: '20', id: 2},
+      {label: 'Score 3', total: '100', id: 3}
+    ]
+  }
+}
+
+    addAccount = (label) => {
+        const {accounts} = this.state;
+
+        let maxId =  Math.max(...accounts.map((account) => account.id), 0),
+            accountObj = {
+                label,
+                total: '0',
+                id: ++maxId,
+                active: false
+            }
+
+        this.setState({accounts: [...accounts, accountObj]});
+    }
+
+render() {
+    const {accounts} = this.state;
+
+    return (
+        <div className="app-header">
+            <h1>Income and expense accounting application</h1>
+            <h3>Balance</h3>
+            <Accounts accountsList={accounts} addAccount={this.addAccount}/>
+            <h3>Account transactions: </h3>
+            <Operation/>
+            <Scoretransfer/>
+        </div>
+    );
+}
+}
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
