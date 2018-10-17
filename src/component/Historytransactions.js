a4b44f40aef80b61b4b3739e379d55ab97fbf119
eukartsan <eugenekartsan@gmail.com>
2018-10-17 16:27:18 +0300

    table transactions2

--- src/component/Historytransactions.js
+++ src/component/Historytransactions.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
 const Historytransactions = (props) => {
-  const content = props.posts.map((post) =>
-      <div key={post.id}>
-        <post.amont>
-      </div>
-    );
+
       return (
           <div>
           <tr>
               <td>{props.amount}</td>
+          </tr>
+            <tr>
               <td>{props.accountId}</td>
-              {content}
           </tr>
           </div>
       )
