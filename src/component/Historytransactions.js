fc0692f19f92fc1de794e8301bda00bb2094075f
eukartsan <eugenekartsan@gmail.com>
2018-10-18 13:53:14 +0300

    transanction with props

--- src/component/Historytransactions.js
+++ src/component/Historytransactions.js
@@ -1,30 +1,21 @@
 import React from 'react';
 import './HistoryTransactions.css';
 
-
 export default class HistoryTransactions extends React.Component{
-  constructor(props){
-    super(props);
-
-  this.state = {
-    transactions: props.transactions
-  };
-}
-
-render(){
+render() {
       return (
 
         <div className="transanctions-container">
-
-        {this.state.transactions.map((i, index) => {
-          return
+        <div className="tableElement">
+            {this.props.accountId}
+        </div>
           <div className="tableElement">
-              {i.amount}
-              {i.commentValue}
+              {this.props.amount}
           </div>
-        })}
+          <div className="tableElement">
+              {this.props.commentValue}
+          </div>
         </div>
       )
-    }
-
   }
+}
