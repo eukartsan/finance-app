e2bdfa5faa02f90eadede16add6e45ee5f1bb785
eukartsan <eugenekartsan@gmail.com>
2018-10-31 16:58:43 +0200

    category add function

--- src/component/NewCategory/NewCategory.js
+++ src/component/NewCategory/NewCategory.js
@@ -6,7 +6,8 @@ export default class NewCategory extends React.Component {
 
         this.state = {
           newIsIncomeChecked: false,
-          menuOpen: true
+          menuCategoryOpen: true,
+          categoryName: ''
         }
       }
 
@@ -28,29 +29,42 @@ export default class NewCategory extends React.Component {
   }
 
   categoryMenuOpen = () => {
-    this.setState(prevState => ({
-      menuOpen: !prevState.menuOpen
-    }));
+      this.setState(({menuCategoryOpen}) => ({menuCategoryOpen: !menuCategoryOpen}));
+  }
+
+  handleChange = (event) => {
+      this.setState({
+          categoryName: event.target.value
+      });
   }
 
   render() {
     const {categories} = this.props;
-    const {menuOpen} = this.state;
+    const {menuCategoryOpen, categoryName} = this.state;
 
-    const newCategoryMenu = menuOpen &&
+    const newCategoryMenu = menuCategoryOpen &&
         <div className="transaction-amount">
           <label>Enter new category:</label>
-          <input placeholder="Category" />
-          <div><input type="checkbox"
+          <input
+            placeholder="Category"
+            name="newAccountName"
+            type="text"
+            value={categoryName}
+            onChange={this.handleChange}
+            />
+          <div>
+          <form>
+          <input type="checkbox"
                  id="income"
                  name="transaction"
                  value="income"
                  onChange={this.newIncomeChange}
                  />
-              <label htmlFor="income" className="label label-checkbox"> Is income?</label>
+               <label htmlFor="income" className="label label-checkbox"> Is income?</label>
+          </form>
               </div>
             <div className="transaction-buttons">
-              <button onChange={this.addNewCategory}>Save</button>
+              <button type="submit" value="Submit" onChange={this.addNewCategory}>Save</button>
             </div>
           </div>
 
@@ -58,7 +72,7 @@ export default class NewCategory extends React.Component {
       <div className="operation-list list-group-item">
         <h3>Create new category: </h3>
         <button onClick={this.categoryMenuOpen}>
-          {menuOpen ? 'Close' : 'Open'}
+          {menuCategoryOpen ? 'Close' : 'Open'}
         </button>
         <form className="transaction-amount mb-8">
             <div>
